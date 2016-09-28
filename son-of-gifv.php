7babe99580ce6471d317a2afb566196ad4e4aea0
pete@petenelson.com <petenelson@fortress.local>
2016-09-28 18:58:48 -0500

    initial commit

---
+++ son-of-gifv.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Son of GIFV
+ * Description: GIF to GIFV converter
+ * Version:     1.0.0
+ * Author:      Pete Nelson pete@petenelson.com
+ * Author URI:  https://petenelson.io
+ *
+ */
+
+function son_of_gifv_imgr_upload() {
+
+	$local_file = dirname( __FILE__ ). '/giddy.gif'; //path to a local file on your server
+
+	$encoded_file = base64_encode(file_get_contents($local_file));
+
+
+	$post_fields = array (
+
+	);
+
+	$response = wp_remote_post( 'https://api.imgur.com/3/image',
+	 array(
+	 	'headers'    => array( 'Authorization' => 'Client-ID d36e23f2ed35b96' ),
+		'body'       => array( 'image' => $encoded_file ),
+	 )
+	);
+
+	var_dump( $response );
+
+}
+
+WP_CLI::add_command( 'gifv upload', 'son_of_gifv_imgr_upload' );
