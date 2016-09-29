070f020e404be11f3d21a1c5fdd94a5e7becfba7
Pete Nelson <pete@petenelson.com>
2016-09-29 17:24:46 -0500

    added is_animated_gif()

--- includes/class-son-of-gifv-attachment.php
+++ includes/class-son-of-gifv-attachment.php
@@ -24,9 +24,15 @@ if ( ! class_exists( 'Son_of_GIFV_Attachment' ) ) {
 				return false;
 			}
 
-			// TODO start here
-			if(!($fh = @fopen($filename, 'rb')))
-			return false;
+			$filename = get_attached_file( $attachment_id );
+
+			// From https://stackoverflow.com/questions/280658/can-i-detect-animated-gifs-using-php-and-gd
+
+			// Open the file.
+			$fh = @fopen( $filename, 'rb' );
+			if( ! $fh ) {
+				return false;
+			}
 			$count = 0;
 			//an animated gif contains multiple "frames", with each frame having a
 			//header made up of:
@@ -36,14 +42,12 @@ if ( ! class_exists( 'Son_of_GIFV_Attachment' ) ) {
 
 			// We read through the file til we reach the end of the file, or we've found
 			// at least 2 frame headers
-			while(!feof($fh) && $count < 2) {
-			$chunk = fread($fh, 1024 * 100); //read 100kb at a time
-			$count += preg_match_all('#\x00\x21\xF9\x04.{4}\x00[\x2C\x21]#s', $chunk, $matches);
+			while( !feof( $fh ) && $count < 2 ) {
+				$chunk = fread( $fh, 1024 * 100 ); //read 100kb at a time
+				$count += preg_match_all( '#\x00\x21\xF9\x04.{4}\x00[\x2C\x21]#s', $chunk, $matches );
 			}
-
-			fclose($fh);
+			fclose( $fh );
 			return $count > 1;
-
 		}
 
 		static public function download_mp4( $url ) {
--- includes/class-son-of-gifv-converter.php
+++ includes/class-son-of-gifv-converter.php
@@ -10,21 +10,31 @@ if ( ! class_exists( 'Son_of_GIFV_Converter' ) ) {
 
 		}
 
-		static public function gif_to_gifv( $attachment_id ) {
-			// TODO
-			// does it already have a GIFV?
-
-			// Setup default results.
-			$results = array(
+		/**
+		 * Returns the default results for the conversion process.
+		 *
+		 * @return array
+		 */
+		static public function get_default_results() {
+			return array(
 				'attachment_id'            => 0,
 				'is_gif'                   => false,
 				'is_animated_gif'          => false,
+				'filename'                 => '',
 				'imgur_id'                 => '',
 				'imgur_results'            => false,
 				'mp4_attachment_id'        => 0,
 				'thumnail_attachment_id'   => 0,
 				'error'                    => '',
-				);
+			);
+		}
+
+		static public function gif_to_gifv( $attachment_id ) {
+			// TODO
+			// does it already have a GIFV?
+
+			// Setup default results.
+			$results = self::get_default_results();
 
 			// Verify it's an attachment.
 			$post = get_post( $attachment_id );
@@ -44,28 +54,33 @@ if ( ! class_exists( 'Son_of_GIFV_Converter' ) ) {
 
 			// Is it a GIF?
 			$results['is_gif'] = Son_of_GIFV_Attachment::is_gif( $attachment_id );
-			if ( $results['is_gif'] ) {
-				$results['attachment_id'] = absint( $attachment_id );
-			} else {
+			if ( ! $results['is_gif'] ) {
 				$results['error'] = sprintf( __( 'Attachment ID %d is not a GIF', 'son-of-gifv' ), $attachment_id );
 				return $results;
 			}
 
 			// Is it an animated GIF
+			$results['is_animated_gif'] = Son_of_GIFV_Attachment::is_animated_gif( $attachment_id );
+			if ( ! $results['is_animated_gif'] ) {
+				$results['error'] = sprintf( __( 'Attachment ID %d is not an animated GIF', 'son-of-gifv' ), $attachment_id );
+				return $results;
+			}
 
-
+			// So far, so good.
+			$results['filename'] = get_attached_file( $attachment_id );
 
 			// upload GIF to Imgur
-			// download MP4
-			// sideload MP4
-			// download thumbnail
-			// flag GIF with GIFV info
+
+			// TODO download MP4
+			// TODO sideload MP4
+			// TODO download thumbnail
+			// TODO flag GIF with GIFV info
 
 			return $results;
 
 		}
 
-		static public function upload_gif_to_imgur( $attachment_id ) {
+		static public function upload_gif_to_imgur( $filename ) {
 			// TODO
 		}
 
