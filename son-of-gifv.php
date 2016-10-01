17c5ed5697aea466e1dc4c555c410885c05d3e41
Pete Nelson <pete@petenelson.com>
2016-10-01 17:21:52 -0500

    fixed include paths

--- son-of-gifv.php
+++ son-of-gifv.php
@@ -27,18 +27,18 @@ if ( ! defined( 'SON_OF_GIFV_URL_ROOT' ) ) {
 }
 
 // Load plugin files.
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-common.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-admin.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-permalinks.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-template.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-attachment.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-converter.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-rest-api.php';
-require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-imgur.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-common.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-admin.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-permalinks.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-template.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-attachment.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-converter.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-rest-api.php';
+require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-imgur.php';
 
 // Load CLI commands
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once SON_OF_GIFV_ROOT . 'includes\class-son-of-gifv-cli.php';
+	require_once SON_OF_GIFV_ROOT . 'includes/class-son-of-gifv-cli.php';
 }
 
 // Initialize plugin code.
