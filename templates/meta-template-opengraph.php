6a279d50adc19a456be959d25366f85337de5735
Pete Nelson <pete@petenelson.com>
2016-10-01 18:53:34 -0500

    updates to gifv template

--- templates/meta-template-opengraph.php
+++ templates/meta-template-opengraph.php
@@ -5,7 +5,8 @@
 <meta property="og:title"        content="<?php echo esc_attr( get_the_title() ); ?>" />
 <meta property="og:url"          content="<?php echo esc_url(  $gifv_data['mp4_url'] ); ?>" />
 <meta property="og:description"  content="<?php echo esc_attr( $gifv_data['caption'] ); ?>" />
-<meta property="og:image"        content="<?php echo esc_url(  $gifv_data['thumbnail_url'] ); ?>" />
+<meta property="og:image" content="<?php echo esc_url(  $gifv_data['thumbnail_url'] ); ?>" />
+<meta property="og:image:secure_url" content="<?php echo esc_url(  $gifv_data['thumbnail_url'] ); ?>" />
 <meta property="og:image:width"  content="<?php echo esc_attr( $gifv_data['attachment_width'] ); ?>" />
 <meta property="og:image:height" content="<?php echo esc_attr( $gifv_data['attachment_height'] ); ?>" />
 <meta property="og:video"        content="<?php echo esc_url(  $gifv_data['mp4_url'] ); ?>" />
