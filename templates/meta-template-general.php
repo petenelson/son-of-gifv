982e32af3e75bdff88446fe58d2b90ce6d00da21
Pete Nelson <pete@petenelson.com>
2016-10-01 18:18:17 -0500

    updates to gifv template

--- templates/meta-template-general.php
+++ templates/meta-template-general.php
@@ -1,1 +1,2 @@
+<meta id="viewport" name="viewport" content="<?php echo esc_attr( 'width=' . $gifv_data['attachment_width'] . ', user-scalable=yes' )?>" />
 <link rel="canonical" href="<?php echo esc_attr( $gifv_data['permalink'] ); ?>" />
--- templates/meta-template-twitter.php
+++ templates/meta-template-twitter.php
@@ -5,11 +5,14 @@
 <meta name="twitter:site"                       content="@gungeekatx" />
 <meta name="twitter:card"                       content="player" />
 <meta name="twitter:domain"                     content="<?php echo esc_attr( $gifv_data['domain'] ); ?>" />
+<meta name="twitter:url"                        content="<?php echo esc_attr( $gifv_data['permalink'] ); ?>" />
 <meta name="twitter:title"                      content="<?php echo esc_attr( get_the_title() ); ?>" />
 <meta name="twitter:description"                content="<?php echo esc_attr( $gifv_data['caption'] ); ?>" />
 <meta name="twitter:image"                      content="<?php echo esc_attr( $gifv_data['thumbnail_url'] ); ?>" />
 <meta name="twitter:player"                     content="<?php echo esc_attr( $gifv_data['permalink'] ); ?>" />
 <meta name="twitter:player:width"               content="<?php echo esc_attr( $gifv_data['attachment_width'] ); ?>" />
 <meta name="twitter:player:height"              content="<?php echo esc_attr( $gifv_data['attachment_height'] ); ?>" />
+<!--
 <meta name="twitter:player:stream"              content="<?php echo esc_attr( $gifv_data['mp4_url'] ); ?>" />
 <meta name="twitter:player:stream:content_type" content="video/mp4">
+-->
