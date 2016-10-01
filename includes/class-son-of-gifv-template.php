4db5b5cb96b08a19b2446757cc23eea37d3a736c
Pete Nelson <pete@petenelson.com>
2016-09-30 19:35:07 -0500

    initial work on Twitter meta tags

--- includes/class-son-of-gifv-template.php
+++ includes/class-son-of-gifv-template.php
@@ -31,14 +31,17 @@ if ( ! class_exists( 'Son_of_GIFV_Template' ) ) {
 		static public function get_template_data( $attachment_id ) {
 
 			$data = array(
+				'id'                 => $attachment_id,
 				'mp4_id'             => get_post_meta( $attachment_id, 'son_of_gifv_mp4_id', true ),
 				'thumbnail_id'       => get_post_meta( $attachment_id, 'son_of_gifv_thumbnail_id', true ),
 				'mp4_url'            => '',
 				'thumbnail_url'      => '',
 				'attachment_width'   => 320,
 				'attachment_height'  => 200,
+				'permalink'          => Son_of_GIFV_Attachment::gifv_url( $attachment_id ),
+				'caption'            => get_the_excerpt( $attachment_id ),
+				'domain'             => str_replace( 'http://', '', str_replace( 'https://', '', get_site_url() ) ),
 				);
-
 
 			if ( ! empty( $data['mp4_id'] ) ) {
 				$data['mp4_url'] = wp_get_attachment_url( $data['mp4_id'] );
@@ -48,10 +51,10 @@ if ( ! class_exists( 'Son_of_GIFV_Template' ) ) {
 				$data['thumbnail_url'] = wp_get_attachment_url( $data['thumbnail_id'] );
 			}
 
-			$attachment_src = wp_get_attachment_image_src( $attachment_id, 'full' );
-			if ( ! empty( $attachment_src ) ) {
-				$data['attachment_width']  = $attachment_src[1];
-				$data['attachment_height'] = $attachment_src[2];
+			$metadata = wp_get_attachment_metadata( $attachment_id );
+			if ( ! empty( $metadata ) ) {
+				$data['attachment_width']  = $metadata['width'];
+				$data['attachment_height'] = $metadata['height'];
 			}
 
 			return $data;
--- templates/gifv.php
+++ templates/gifv.php
@@ -2,19 +2,26 @@
 
 	// Get the GIFV information.
 
-	$data = Son_of_GIFV_Template::get_template_data( get_the_id() );
+	$gifv_data = Son_of_GIFV_Template::get_template_data( get_the_id() );
+
+	$twitter_meta_template  = apply_filters( 'son-of-gifv-twitter-meta-template', SON_OF_GIFV_PATH . 'templates/meta-template-twitter.php' );
+	$facebook_meta_template = apply_filters( 'son-of-gifv-facebook-meta-template', SON_OF_GIFV_PATH . './templates/meta-template-facebook.php' );
 
 ?>
 <html>
 	<head>
 		<title><?php the_title(); ?></title>
+
+		<?php if ( file_exists( $twitter_meta_template ) ) { include_once $twitter_meta_template; } ?>
+		<?php if ( file_exists( $facebook_meta_template ) ) { include_once $facebook_meta_template; } ?>
+
 	</head>
 	<body style="margin: 0; padding: 0;">
 
-	<!-- <?php var_dump( $data ); ?> -->
+	<!-- <?php var_dump( $gifv_data ); ?> -->
 
-	<video width="<?php echo esc_attr( $data['attachment_width'] ); ?>" height="<?php echo esc_attr( $data['attachment_height'] ); ?>" autoplay loop>
-		<source src="<?php echo esc_url( $data['mp4_url'] ); ?>" type="video/mp4">
+	<video width="<?php echo esc_attr( $gifv_data['attachment_width'] ); ?>" height="<?php echo esc_attr( $gifv_data['attachment_height'] ); ?>" autoplay loop>
+		<source src="<?php echo esc_url( $gifv_data['mp4_url'] ); ?>" type="video/mp4">
 	</video>
 
 	</body>
